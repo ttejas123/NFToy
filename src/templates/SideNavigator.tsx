460c3b4beac61ee8ccbd39ff45b55a261a487819
Tejas <tthakare73@gmail.com>
2023-01-08 23:40:59 +0530

    Completed Landing page

--- src/templates/SideNavigator.tsx
+++ src/templates/SideNavigator.tsx
@@ -4,68 +4,31 @@ import Link from 'next/link';
 
 function SideNavigator() {
   const [active, setActive] = useState(0);
+  const links: string[] = [
+    'HOME',
+    'BUILD',
+    'WHYUS',
+    'OURWORK',
+    'TRUSTEDCOMMUNITY',
+  ];
   return (
     <div className="fixed right-7 z-10 top-1/4 hidden md:block">
-      <Link href="#HOME">
-        <div
-          onClick={() => setActive(0)}
-          className="border border-black rounded-full w-4 h-4 flex justify-center items-center my-2 cursor-pointer"
-        >
-          <div
-            className={`${
-              active !== 0 && 'hidden'
-            } w-2 h-2 bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow rounded-full`}
-          ></div>
-        </div>
-      </Link>
-      <Link href="#BUILD">
-        <div
-          onClick={() => setActive(1)}
-          className="border border-black rounded-full w-4 h-4 flex justify-center items-center my-2 cursor-pointer"
-        >
-          <div
-            className={`${
-              active !== 1 && 'hidden'
-            } w-2 h-2 bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow rounded-full`}
-          ></div>
-        </div>
-      </Link>
-      <Link href="#WHYUS">
-        <div
-          onClick={() => setActive(2)}
-          className="border border-black rounded-full w-4 h-4 flex justify-center items-center my-2 cursor-pointer"
-        >
-          <div
-            className={`${
-              active !== 2 && 'hidden'
-            } w-2 h-2 bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow rounded-full`}
-          ></div>
-        </div>
-      </Link>
-      <Link href="#OURWORK">
-        <div
-          onClick={() => setActive(3)}
-          className="border border-black rounded-full w-4 h-4 flex justify-center items-center my-2 cursor-pointer"
-        >
-          <div
-            className={`${
-              active !== 3 && 'hidden'
-            } w-2 h-2 bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow rounded-full`}
-          ></div>
-        </div>
-      </Link>
-      <Link href="#TRUSTEDCOMMUNITY">
-        <div
-          onClick={() => setActive(4)}
-          className="border border-black rounded-full w-4 h-4 flex justify-center items-center my-2 cursor-pointer"
-        >
-          <div
-            className={`${
-              active !== 4 && 'hidden'
-            } w-2 h-2 bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow rounded-full`}
-          ></div>
-        </div>
-      </Link>
+      {links.map((val: string, index: number) => {
+        return (
+          <Link href={`#${val}`} key={index}>
+            <div
+              onClick={() => setActive(index)}
+              className="border border-black rounded-full w-4 h-4 flex justify-center items-center my-2 cursor-pointer"
+            >
+              <div
+                className={`${
+                  active !== index && 'hidden'
+                } w-2 h-2 bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow rounded-full`}
+              ></div>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
